e32fc464b1e6bc7e3a68effc0966ce9224a6a044
hankertrix <91734413+hankertrix@users.noreply.github.com>
2022-10-04 14:55:56 +0000

    Changed the server url in the keep_bot_alive function.

--- main.py
+++ main.py
@@ -1746,7 +1746,7 @@ def keep_bot_alive() -> None:
             try:
 
                 # Gets the bot url
-                s.get("https://bible-verses-bot.onrender.com")
+                s.get("https://bible-verses-bot-7h61.onrender.com")
 
                 # Breaks the loop if its successful
                 break
