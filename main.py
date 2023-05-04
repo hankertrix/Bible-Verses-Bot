96bf330ef808f0605409a45730be0742d0171fa8
hankertrix <91734413+hankertrix@users.noreply.github.com>
2023-05-04 03:22:27 +0000

    Removed the thread details from the debug message.

--- main.py
+++ main.py
@@ -1114,7 +1114,7 @@ def debug(message: types.Message) -> None:
     if message.chat.id != int(os.environ["DEV_ID"]):
         return
 
-    msg = f'db["subbed"] = {db["subbed"]}\n\ndb["chats_version"] = {db["chats_version"]}\n\ndb["previous_sent_time"] = {repr(db["previous_sent_time"])}\n\n{TimeCheck.instances[0].__dict__}'
+    msg = f'db["subbed"] = {db["subbed"]}\n\ndb["chats_version"] = {db["chats_version"]}\n\ndb["previous_sent_time"] = {repr(db["previous_sent_time"])}'
     split_message(message, msg)
 
 # Name safeguard
