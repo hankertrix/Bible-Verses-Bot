1c964c087921f2aa2c835e500b19629bb7b5ad9a
hankertrix <91734413+hankertrix@users.noreply.github.com>
2024-11-24 12:25:57 +0800

    Serve on port 8080

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,21 @@
 # Module to keep the bot alive
 
-from flask import Flask
-from waitress import serve
 from threading import Thread
 
-app = Flask('')
+from flask import Flask
+from waitress import serve
 
-@app.route('/')
+app = Flask("")
+
+
+@app.route("/")
 def main() -> str:
     return "Your bot is alive!"
 
+
 def run() -> None:
-    serve(app, host="0.0.0.0", port=10000)
+    serve(app, host="0.0.0.0", port=8080)
+
 
 def keep_alive() -> None:
     server = Thread(target=run, daemon=True)
