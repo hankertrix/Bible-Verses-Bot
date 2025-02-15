9bacd38e4c62e596fbe4656e8b0dfefe09a87f78
hankertrix <91734413+hankertrix@users.noreply.github.com>
2025-02-15 13:35:14 +0800

    Updated the headers to something more recent

--- request_sess.py
+++ request_sess.py
@@ -4,7 +4,7 @@ import httpx
 
 # Request headers
 headers = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3",
+  "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.3",
   "Accept-Language": "en-US,en;q=0.9",
   "DNT": "1",
   "Referer": "https://www.google.com/"
